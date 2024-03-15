9afb0253b82c976813957e06b3c8af5667bad571
Suvrayan Bandyopadhyay <suvrayanb@gmail.com>
2024-03-15 23:55:28 +0530

    Initial file structure and main.c created

---
+++ Source/Main.c
@@ -0,0 +1,9 @@
+//This is to supress depreciation errors which might happen since we are working with C
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() 
+{
+	printf("Hello World");
+	return 0;
+}
