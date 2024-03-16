25bd051fd674b663f6dcfbd77a2b7a06d0a0f8f3
Suvrayan Bandyopadhyay <suvrayanb@gmail.com>
2024-03-16 12:00:01 +0530

    Added a blank raylib screen

--- Source/Main.c
+++ Source/Main.c
@@ -1,9 +1,35 @@
 //This is to supress depreciation errors which might happen since we are working with C
 #define _CRT_SECURE_NO_WARNINGS
+#include"raylib.h"
 #include<stdio.h>
 
-int main() 
+int main(void)
 {
-	printf("Hello World");
-	return 0;
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("WINDOW", 380, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
